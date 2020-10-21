505326e8b5d1b8acfc1a6bb3ecc00b5238d70c91
KarenCortesT <cortestelloa@gmail.com>
2020-10-21 14:58:12 -0500

    Updates

---
+++ precache-manifest.e8fcc360556a60af7cd7bc3de0d89cc5.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "496ef67f3824ab67f4ad1db736e7115f",
+    "url": "/webAT/index.html"
+  },
+  {
+    "revision": "8ec6e3d94ead8a4fbddd",
+    "url": "/webAT/static/css/main.e02690cb.chunk.css"
+  },
+  {
+    "revision": "c7019fd520405952ee1d",
+    "url": "/webAT/static/js/2.656e65ce.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/webAT/static/js/2.656e65ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ec6e3d94ead8a4fbddd",
+    "url": "/webAT/static/js/main.46a01d79.chunk.js"
+  },
+  {
+    "revision": "7f53bb9ec5b58096357a",
+    "url": "/webAT/static/js/runtime-main.f8cea0e0.js"
+  },
+  {
+    "revision": "cf70eee52f1df295563b6728b96237b4",
+    "url": "/webAT/static/media/David.cf70eee5.png"
+  },
+  {
+    "revision": "128dc149e5081909f3a62cc5324d0601",
+    "url": "/webAT/static/media/Diseño.128dc149.png"
+  },
+  {
+    "revision": "10eff3664f26cf25297fb38d7cecceb6",
+    "url": "/webAT/static/media/MiPOT.10eff366.png"
+  },
+  {
+    "revision": "b75112d46f6f54ae1e6133dae7fd50cb",
+    "url": "/webAT/static/media/MiPOT_VM.b75112d4.png"
+  },
+  {
+    "revision": "ef8da6d0fe77410793d480ee4de407d3",
+    "url": "/webAT/static/media/Miguel.ef8da6d0.png"
+  },
+  {
+    "revision": "a24d09794fc9532f789a2ff0186c23b4",
+    "url": "/webAT/static/media/Reivaj.a24d0979.png"
+  },
+  {
+    "revision": "d85636e6a97f7df2a395aab62f9a1ac0",
+    "url": "/webAT/static/media/SoftwareApp.d85636e6.png"
+  },
+  {
+    "revision": "2bf55b10d12903f465e0066faf4cbb05",
+    "url": "/webAT/static/media/SoftwareWeb.2bf55b10.png"
+  },
+  {
+    "revision": "ffbee7f4a32164cd742027b42a4a40a4",
+    "url": "/webAT/static/media/Team.ffbee7f4.png"
+  },
+  {
+    "revision": "6ef5a987d1f5bae16658f474638f572a",
+    "url": "/webAT/static/media/foto.6ef5a987.png"
+  },
+  {
+    "revision": "d1a45d9552f82220cb4f778fe5f38e91",
+    "url": "/webAT/static/media/iphone_BG.d1a45d95.png"
+  },
+  {
+    "revision": "ed7f36b95efa5ca786952fa793c984ea",
+    "url": "/webAT/static/media/iphone_responsive.ed7f36b9.png"
+  },
+  {
+    "revision": "f6ce88f5521a8f65664b6e1ae33e8b3c",
+    "url": "/webAT/static/media/unete.f6ce88f5.png"
+  }
+]);
